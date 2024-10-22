56896979e1183719b7eee863f1cd8561a55276a8
Alan.S <alan.silva-17@hotmail.com>
2024-10-21 22:23:28 -0300

    states/campo

--- rev-effect/src/app/page.tsx
+++ rev-effect/src/app/page.tsx
@@ -4,22 +4,26 @@ import { useState } from "react";
 
 
 const Page = () => {
-  const [showSecret,SetShowSecret]= useState(false)
+  const [nameInput, setNameInput] = useState('joao');
 
-  const handleClickButton = () => {
-    SetShowSecret(!showSecret)   
-  }
+  const handleBtnClick = () => {
+      alert(nameInput)
+  }    
+  
 
 
     return (
-      <div className="w-screen h-screen flex flex-col justify-center items-center text-3xl">
-        <button onClick={handleClickButton} className="bg-blue-500 p3 rounded-md">{showSecret ? 'Ocultar' : 'Mostrar'}</button>
-        {showSecret &&        
-           <div className="bg-blue-500 rounded-md p-3 mt-5">Área Secreta</div>
-        }
-      </div>
-
-      
+        <div className="w-screen h-screen flex flex-col justify-center items-center text-3xl">
+            <input 
+            type="text"
+            className="border  border-black p-3 text-xl text-black rounded" 
+            placeholder="Digite seu nome"
+            value={nameInput}
+            onChange={e => setNameInput(e.target.value)}
+            />
+            <p>Seu nome é:{nameInput}</p>
+            <button onClick={handleBtnClick}>Mostrar Valor do campo</button>
+        </div>
     );
 }
 
